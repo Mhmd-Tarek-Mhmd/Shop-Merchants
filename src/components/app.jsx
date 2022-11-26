01d639867c5481276cc372d0e710623773a04181
Mhmd <mhmd.tarek197@gmail.com>
2022-11-27 00:13:28 +0200

    ADD: feedback component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,15 @@
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 
+import Feedback from "./feedback";
+
 const theme = createTheme();
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <Feedback />
     </ThemeProvider>
   );
 }
